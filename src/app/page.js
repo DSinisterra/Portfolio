b6c4ab4f5e4e15dc52837ab3d80f2dbfb4e13eef
Denis Sinisterra <denissinisterra@gmail.com>
2023-08-20 21:39:23 +1000

    icons in navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,11 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import NavBar from '@/componentes/NavBar';
+import '@fortawesome/fontawesome-svg-core/styles.css';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <NavBar />
     </main>
   )
 }
--- src/componentes/NavBar/index.js
+++ src/componentes/NavBar/index.js
@@ -1,10 +1,23 @@
-import '@fortawesome/fontawesome-free/css/all.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter, faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 export default function NavBar() {
     return (
-      <section className="flex flex-col items-center justify-between p-24">
+      <nav className="w-full h-14 fixed z-50 flex justify-between items-center px-5">
         <h1>Denis</h1>
-        <div></div>
-      </section>
+        <section className=''>
+          <a href='https://github.com/DSinisterra/' target='_blank' rel="noopener noreferrer" className='' >
+            <FontAwesomeIcon icon={faGithub} size="lg" />
+          </a>
+
+          <a href='https://www.linkedin.com/in/denis-sinisterra-9bb49b218/' target='_blank' rel="noopener noreferrer" className='' >
+            <FontAwesomeIcon icon={faLinkedinIn} size="lg" />
+          </a>
+
+          <a href='https://twitter.com/dsinisterra_' target='_blank' rel="noopener noreferrer" className='' >
+            <FontAwesomeIcon icon={faTwitter} size="lg" />
+          </a>
+        </section>
+      </nav>
     )
   }
