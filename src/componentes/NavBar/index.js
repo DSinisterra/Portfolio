bb4302d8594bffa6c816bfd2f2c8627b6c5bc169
Denis Sinisterra <denissinisterra@gmail.com>
2023-08-20 20:51:08 +1000

    first commit

---
+++ src/componentes/NavBar/index.js
@@ -0,0 +1,10 @@
+import '@fortawesome/fontawesome-free/css/all.css';
+
+export default function NavBar() {
+    return (
+      <section className="flex flex-col items-center justify-between p-24">
+        <h1>Denis</h1>
+        <div></div>
+      </section>
+    )
+  }
